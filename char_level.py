1b925f4ae2eb88d0e18d76cb8f4c00addb629743
reese <reesedporter@gmail.com>
2021-07-11 13:52:24 -0500

    typos

--- char_level.py
+++ char_level.py
@@ -6,14 +6,14 @@ from time import sleep
 NGRAM_LEVEL = 40
 
 def tokenize(words: str):
-    '''split into char level tokens based on spaces, strip punctuation'''
+    '''split into char level tokens'''
     return [c for c in words if not c == "\n"]
 
 def ngrams(words: str):
     '''generate ngrams from a string'''
     words = tokenize(words)
     ngrams = {}
-    for i in range(len(words) - 3):
+    for i in range(len(words) - NGRAM_LEVEL):
         if not ngrams.get(words[i], False):
             ngrams[words[i]] = []
         for j in range(1, NGRAM_LEVEL):
@@ -27,25 +27,17 @@ def create_quote(tokens: list, grams: dict):
     last_word = choice(tokens)
     quote = last_word.capitalize()
     for i in range(randint(30, 45)):
-        last_word = choice(grams[last_word])
+        last_word = choice(grams[last_word[-1]])
         quote += last_word
-        last_word = last_word[-1]
     return quote
 
 
 def main():
-    '''read words and generate quotes every five seconds'''
+    '''read words and generate quote'''
     with open('words.txt', 'r') as f:
         words = f.read()
     tokens, grams = ngrams(words)
-    while True:
-        try:
-            print(create_quote(tokens, grams))
-            print()
-            sleep(5)
-        except KeyboardInterrupt: 
-            break
-
+    print(create_quote(tokens, grams))
 
 if __name__ == '__main__':
     main()
